6b27dd2b6301ac59b3e0fdc2c5f377b77fca0a26
Ran Pol <rnd809@gmail.com>
2023-05-12 19:06:52 -0400

    feat: Add About and Services pages.

---
+++ app/about/page.jsx
@@ -0,0 +1,15 @@
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+        perspiciatis, enim ratione, deserunt dolor ad dolore explicabo quis
+        incidunt labore, et illo natus nemo pariatur suscipit dicta quod earum
+        maiores.
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage

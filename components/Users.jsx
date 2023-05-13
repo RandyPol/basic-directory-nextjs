e49edb8493f24a8325f27e45f5ec5a6cbe0aa4d1
Ran Pol <rnd809@gmail.com>
2023-05-13 13:18:09 -0400

    Add Bootstrap styling and classes to Users component.

--- components/Users.jsx
+++ components/Users.jsx
@@ -5,9 +5,13 @@ import Image from 'next/image'
 function Users({ users }) {
   const router = useRouter()
   return (
-    <ul>
+    <ul className="list-group">
       {users.map((user) => (
-        <li key={user.id} onClick={() => router.push(`/users/${user.id}`)}>
+        <li
+          key={user.id}
+          onClick={() => router.push(`/users/${user.id}`)}
+          className="list-group-item d-flex justify-content-between align-items-center list-group-item-action"
+        >
           <div>
             <h5>
               {user.id} {user.first_name} {user.last_name}
@@ -19,6 +23,7 @@ function Users({ users }) {
             alt={user.first_name}
             width={100}
             height={100}
+            className="rounded-circle"
           />
         </li>
       ))}

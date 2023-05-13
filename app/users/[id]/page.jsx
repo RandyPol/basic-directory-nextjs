75fd140e48365c71c876e758ca44d0ed46bfa508
Ran Pol <rnd809@gmail.com>
2023-05-13 11:45:39 -0400

    feat: add async fetch for user data in UsersPage

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,4 +1,11 @@
-function UsersPage() {
+async function getUser(id) {
+  const res = await fetch(`https://reqres.in/api/users/${id}`)
+  const data = await res.json()
+  return data.data
+}
+async function UsersPage({ params }) {
+  const userData = await getUser(params.id)
+  console.log(userData)
   return (
     <div>
       <h1>Users Details</h1>

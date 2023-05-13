9489a5b0da05b1d8c07fda68d0fcf8aeb8466dcd
Ran Pol <rnd809@gmail.com>
2023-05-13 13:26:13 -0400

    Refactor user details layout and style

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -7,22 +7,24 @@ async function getUser(id) {
 }
 async function UsersPage({ params }) {
   const userData = await getUser(params.id)
-  console.log(userData)
   return (
-    <div>
-      <h1>Users Details</h1>
-      <div>
-        <Image
-          src={userData.avatar}
-          alt={userData.first_name}
-          width={100}
-          height={100}
-        />
-        <div>
-          <h3>
-            {userData.first_name} {userData.last_name}
-          </h3>
-          <p>{userData.email}</p>
+    <div className="row">
+      <div className="col-md-6 offset-md-3">
+        <div className="card">
+          <div className="card-header text-center">
+            <Image
+              src={userData.avatar}
+              alt={userData.first_name}
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className="card-body text-center">
+            <h3>
+              {userData.first_name} {userData.last_name}
+            </h3>
+            <p>{userData.email}</p>
+          </div>
         </div>
       </div>
     </div>

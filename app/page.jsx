0231511132c606d8c89afd6192448ccaff4979c3
Ran Pol <rnd809@gmail.com>
2023-05-13 00:13:21 -0400

    feat: Add fetchUsers and display users on IndexPage

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,18 @@
-function IndexPage() {
-  return <div>Index Page</div>
+async function fetchUsers() {
+  const res = await fetch('https://reqres.in/api/users?page=1')
+  const data = await res.json()
+  return data.data
+}
+
+async function IndexPage() {
+  const users = await fetchUsers()
+
+  return (
+    <div>
+      <h1>Index Page</h1>
+      <div>{JSON.stringify(users)}</div>
+    </div>
+  )
 }
 
 export default IndexPage

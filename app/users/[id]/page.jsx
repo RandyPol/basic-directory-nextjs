b14c59fe2e48311f05898f139a11c2818d9a807f
Ran Pol <rnd809@gmail.com>
2023-05-13 11:55:09 -0400

    feat: add user avatar and details to UsersPage component

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 async function getUser(id) {
   const res = await fetch(`https://reqres.in/api/users/${id}`)
   const data = await res.json()
@@ -9,6 +11,20 @@ async function UsersPage({ params }) {
   return (
     <div>
       <h1>Users Details</h1>
+      <div>
+        <Image
+          src={userData.avatar}
+          alt={userData.first_name}
+          width={100}
+          height={100}
+        />
+        <div>
+          <h3>
+            {userData.first_name} {userData.last_name}
+          </h3>
+          <p>{userData.email}</p>
+        </div>
+      </div>
     </div>
   )
 }

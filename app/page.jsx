b37a9816e051b1c5a8a5281336920ef742c87204
Ran Pol <rnd809@gmail.com>
2023-05-13 00:22:33 -0400

    refactor: Update IndexPage to display users as a list with avatar images

--- app/page.jsx
+++ app/page.jsx
@@ -6,11 +6,24 @@ async function fetchUsers() {
 
 async function IndexPage() {
   const users = await fetchUsers()
+  console.log(users)
 
   return (
     <div>
       <h1>Index Page</h1>
-      <div>{JSON.stringify(users)}</div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <div>
+              <h5>
+                {user.id} {user.first_name} {user.last_name}
+              </h5>
+              <p>{user.email}</p>
+            </div>
+            <img src={user.avatar} alt={user.first_name} />
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
